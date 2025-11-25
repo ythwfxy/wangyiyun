78584fc28e884bf58688331b3334a7d49cdfcec5
ythwfxy <1832164480@qq.com>
2025-11-25 16:01:02 +0800

    1215

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -107,6 +107,12 @@ const router = new VueRouter({
                 path:'/home/myFM',
                 component:()=>
                     import('@/pages/myFM/myFM')
+            },
+            // 音乐时光机
+            {
+                path:'/home/music-time-machine',
+                component:()=>
+                    import('@/pages/musicTimeMachine/musicTimeMachine')
             }
         ]
     }]
