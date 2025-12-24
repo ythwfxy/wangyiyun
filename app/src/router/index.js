78171a12ff632fcf6a9907b815f27dec0d5066f0
ythwfxy <1832164480@qq.com>
2025-12-24 12:15:14 +0800

    123

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -103,10 +103,22 @@ const router = new VueRouter({
                     import('@/pages/songPage/songPage')
             },
             // 私人FM
-            {
+            { 
                 path:'/home/myFM',
                 component:()=>
                     import('@/pages/myFM/myFM')
+            },
+            // 我的听歌足迹
+            { 
+                path:'/home/listening-footprint',
+                component:()=>
+                    import('@/pages/listeningFootprint/listeningFootprint')
+            },
+            // 音乐备忘录
+            { 
+                path:'/home/music-memo',
+                component:()=>
+                    import('@/pages/musicMemo/musicMemo')
             }
         ]
     }]
